aa507a9fa3e3f3273b500e0a511129b22d590f44
r0HERS <re_rohers@hotmail.com>
2023-10-20 14:04:22 -0300

    Básico de entrada

---
+++ reservance/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+const name ='Reservance'
+
+function App() {
+  return (
+
+    
+
+    <div className="App">
+      <h1>{name}</h1>
+
+    </div>
+  );
+}
+
+export default App;

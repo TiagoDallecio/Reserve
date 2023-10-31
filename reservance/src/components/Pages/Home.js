9339e0c19776b40d6ed6a950f1c3f3d07f33ec9b
r0HERS <re_rohers@hotmail.com>
2023-10-31 20:00:57 -0300

    Home page css

--- reservance/src/components/Pages/Home.js
+++ reservance/src/components/Pages/Home.js
@@ -1,13 +1,21 @@
+import styles from './Home.module.css'
+import imagem from '../../img/restaurantecima.png' 
+import LinkButton from '../Layout/LinkButton'
+
 function Home(props){
 
     return(
 
-        <>
-        <h1>
-            Home
-        </h1>
-        
-        </>
+        <section className={styles.home_container}>
+            <h1>
+                 <span>Reser</span><span1>vance</span1>
+             </h1>    
+                <p>A melhor reserva para a melhor experiência!</p>
+                <LinkButton to="/CriarProjeto" text="Criar Projeto"/>
+                <img src={imagem} alt="Reservance"/>
+           
+
+        </section>
 
     )
 

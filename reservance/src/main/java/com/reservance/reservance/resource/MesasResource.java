2e1a8a01f403e04921388ded7f66dcf4f731fefc
Tiago Oliveira Dallecio <112666497+TiagoDallecio@users.noreply.github.com>
2023-11-01 09:56:32 -0300

    Querys de acesso a mesas específicas de um restaurante (GET, PUT, DELETE)

--- reservance/src/main/java/com/reservance/reservance/resource/MesasResource.java
+++ reservance/src/main/java/com/reservance/reservance/resource/MesasResource.java
@@ -1,9 +1,12 @@
 package com.reservance.reservance.resource;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,8 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.reservance.reservance.model.Mesas;
 import com.reservance.reservance.repository.MesasRepository;
 
+import jakarta.persistence.criteria.Path;
+
 @RestController
-@RequestMapping("/estabelecimentos")
+@RequestMapping("/api")
 public class MesasResource {
 
 	@Autowired
@@ -28,19 +33,34 @@ public class MesasResource {
 		return mr.findAll();
 	}
 	
-	@GetMapping("/mesas/{id}")
-	public List<Mesas> gebyID(@PathVariable Long id){
-		return mr.findByIdEst(id);
-	}
-	
 	@PostMapping("/mesas")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Mesas inserirMesa(@RequestBody Mesas mesa) {
 		return mr.save(mesa);
 	}
 	
-	//@PutMapping("/mesas")
+	@GetMapping("{name}/mesas")
+	public List<Mesas> getByName(@PathVariable String name){
+		return mr.findByEstName(name);
+	}
 	
+	@GetMapping("{name}/mesas/{id}")
+	public Mesas lerMesa(@PathVariable String name, @PathVariable Long id) {
+		return mr.findEstMesa(name,id);
+	}
 	
+	@PutMapping("{name}/mesas/{id}")
+	public Mesas updateMesa(@PathVariable String name, @PathVariable Long id, @RequestBody Mesas mesa) {
+		Mesas mesaAtual = mr.findEstMesa(name, id);
+		BeanUtils.copyProperties(mesa, mesaAtual, "id");
+		
+		return mr.save(mesaAtual);
+	}
+	
+	@DeleteMapping("{name}/mesas/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteMesa(@PathVariable String name, @PathVariable Long id) {
+		mr.deleteEstMesa(name, id);
+	}
 	
 }

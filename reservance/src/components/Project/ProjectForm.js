8bd1a5352fae651fc83eb5575e40fbe0cefd842b
Tiago Oliveira D'alessi <112666497+TiagoDallecio@users.noreply.github.com>
2023-11-28 12:41:27 -0300

    Requisições para o backend com axios.

--- reservance/src/components/Project/ProjectForm.js
+++ reservance/src/components/Project/ProjectForm.js
@@ -1,25 +1,60 @@
-import styles from './ProjectForm.module.css'
+import styles from'./ProjectForm.module.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
+import CriarProjeto from '../Pages/CriarProjeto';
 
 
-function ProjectForm(props){
+
+    const AdicionarEstab = () => {
+
+        const moment = require('moment')
+    
+        const[nome, setNome] = useState('');
+        const[funcionamento_dia, setFuncionamento_dia] = useState('');
+        const[hora_inicio, setHora_inicio] = useState('');
+        const[hora_fim, setHora_fim] = useState('');
+        const[num_mesas, setNum_mesas] = useState('');
+    
+        const hora_inicioStr = moment({hora_inicio}).format('HH:mm')
+        const hora_fimStr = moment({hora_fim}).format('HH:mm')
+    
+        const handleAdicionarEstab = async () => {
+            try{
+                console.log('Iniciando a solicitação...');
+            const EstabResponse = await axios.post('http://localhost:8080/api/estabelecimento', {
+                nome,
+                funcionamento_dia,
+                hora_inicio: hora_inicioStr,
+                hora_fim: hora_fimStr,
+                num_mesas: parseInt(num_mesas)
+            });
+    
+            console.log('Resposta do Backend: ', EstabResponse.data);
+            setNome('');
+            setFuncionamento_dia('');
+            setHora_inicio('');
+            setHora_fim('');
+            setNum_mesas('');
+        } catch (error){
+            console.error('Erro ao enviar os dados:', error);
+        }
+    };
 
     return(
         <form className={styles.form}>
-        <Input type="text" text="Nome do Restaurante" name="Nome" placeholder="Insira o Nome do Restaurante"/>
-        <Input type="number" text="Número de mesas" name="NumeroMesas" placeholder="Número de Mesas"/>
-        <Input type="datetime-local" text="Dias de funcionamento" name="Nome" placeholder="Funcionamento"/>
-        <Input type="time" text="Horário de Abertura" name="Abertura" placeholder="Abertura"/>
-        <Input type="time" text="Horário de Fechamento" name="Fechamento" placeholder="Fechamento"/>
+        <Input type="text" text="Nome do Restaurante" name="Nome" placeholder="Insira o Nome do Restaurante" value = {nome} onChange={(e) => setNome(e.target.value)} />
+        <Input type="number" text="Número de mesas" name="NumeroMesas" placeholder="Número de Mesas" value = {num_mesas} onChange={(e) => setNum_mesas(e.target.value)} />
+        <Input type="text" text="Dias de funcionamento" name="Diasdefuncionamento" placeholder="Funcionamento" value = {funcionamento_dia} onChange={(e) => setFuncionamento_dia(e.target.value)} />
+        <Input type="time" text="Horário de Abertura" name="Abertura" placeholder="Abertura" value = {hora_inicio} onChange={(e) => setHora_inicio(e.target.value)}/>
+        <Input type="time" text="Horário de Fechamento" name="Fechamento" placeholder="Fechamento" value = {hora_fim} onChange={(e) => setHora_fim(e.target.value)} />
         
-        <Select name="category_id" text="Selecione uma categoria"/>
 
-        <SubmitButton text={props.btnText}/>
+        <button onClick={handleAdicionarEstab}>Criar</button>
 
         </form>
-    )
-}
-
-export default ProjectForm
+        )
+    }
+export default AdicionarEstab;
--- reservance/src/components/form/Input.js
+++ reservance/src/components/form/Input.js
@@ -1,14 +1,14 @@
 import styles from './Input.module.css'
 
-function input(props){
+function Input(props){
 
     return(
         <div className={styles.form_control}>
             <label htmlFor={props.name}>{props.text}</label>
-            <input type={props.type} name={props.name} id={props.name} placeholder={props.placeholder} onChange={props.handleOnChange} value={props.value} />
+            <input type={props.type} name={props.name} id={props.name} placeholder={props.placeholder} onChange={props.onChange} value={props.value} />
         </div>
     )
 
 }
 
-export default input
+export default Input;

bdb666d7977646cfc5b310ab485f8a39762ce4c0
r0HERS <re_rohers@hotmail.com>
2023-12-06 11:55:46 -0300

    swipeSlider

--- reservance/src/components/Pages/FormsReserva.js
+++ reservance/src/components/Pages/FormsReserva.js
@@ -1,8 +1,22 @@
+import styles from './FormsReserva.module.css'
+import Input from '../form/Input'
+import LinkButton from '../Layout/LinkButton';
+
 function FormsReserva(){
 
     return(
         <>
+         <form className={styles.form}>
+        <Input type="text" text="Nome" name="nome" placeholder="Insira o seu Nome"/>
+        <Input type="text" text="CPF" name="CPF" placeholder="CPF"/>
+        <Input type="text" text="e-mail" name="email" placeholder="reservance@reservance.com"/>
+        <Input type="text" text="Telefone" name="telefone" placeholder="(--)     -    "/>
+        <Input type="text" text="Observação Extra" name="-observacao" placeholder="Este campo é destinado ao estavelecimento"/>
         
+
+        <LinkButton text="Confirmar"/>
+
+        </form>
         
         </>
     )
--- reservance/src/components/Pages/Home.js
+++ reservance/src/components/Pages/Home.js
@@ -1,11 +1,56 @@
 import styles from './Home.module.css'
-import imagem from '../../img/restaurantecima.png' 
+import imagem from '../../img/pratorestaurante.png' 
+import imagem2 from '../../img/restaurantecima2.png'
+import imagem3 from '../../img/restaurantecima3.png'
+import imagem4 from '../../img/restaurantecima4.png'
+import imagem5 from '../../img/restaurantecima5.png'
+import imagem6 from '../../img/restaurantecima6.png'
+
 import LinkButton from '../Layout/LinkButton'
+import { register } from 'swiper/element/bundle'
+import 'swiper/css'
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import { Swiper, SwiperSlide } from 'swiper/react'
+register();
+
+const imagens =[
+
+    { id:'1', image: imagem},
+    { id:'2', image: imagem2},
+    { id:'3', image: imagem3},
+    { id:'4', image: imagem4},
+    { id:'5', image: imagem5},
+    { id:'6', image: imagem6},
+
+]
+
+
 
 function Home(props){
 
     return(
-
+        <>
+        <Swiper
+            slidesPerView={1}
+            pagination={{ clickable: true }}
+            navigation
+            >
+                {imagens.map( (item) =>(
+                    <SwiperSlide key={item.id}>
+                        <img src={item.image}
+                        alt="slider"
+                        className="slider-item"
+                        style={{
+                            width: '100%',
+                            height: '350px',
+                            objectFit: 'cover',
+                        }}
+                        />
+                    </SwiperSlide>
+                ))}
+        </Swiper>
         <section className={styles.home_container}>
             <h1>
                  <span>Reser</span><span1>vance</span1>
@@ -16,7 +61,7 @@ function Home(props){
            
 
         </section>
-
+        </>
     )
 
 }

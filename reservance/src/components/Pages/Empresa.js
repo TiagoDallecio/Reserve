3186967dc14ce5e96e58cb6c361d4868d5c363d3
r0HERS <re_rohers@hotmail.com>
2023-12-08 08:56:00 -0300

    Reserva mais completa

--- reservance/src/components/Pages/Empresa.js
+++ reservance/src/components/Pages/Empresa.js
@@ -1,13 +1,24 @@
+import styles from './Empresa.module.css'
+import imagem from '../../img/pratorestaurante.png' 
 function Empresa(props){
 
     return(
 
-        <>
+        <div className={styles.empresa_container}>
         <h1>
             Empresa
         </h1>
+        <h2>
+            Criação
+        </h2>
+        <div1>
+        <p1> A Reservance, é um projeto idealizado a partir de um projeto de faculdade, do qual, a vontade
+            e carinho pela idéia, se externalisou e se concretizou nesta empresa.
+        </p1>
+        <img  src={imagem}/>
+        </div1>
+        </div>
         
-        </>
 
     )
 
--- reservance/src/components/Pages/ExecReserva.js
+++ reservance/src/components/Pages/ExecReserva.js
@@ -1,20 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './ExecReserva.module.css';
 import MesaQuatro from '../../img/MesaQuatro.png';
 import MesaDois from '../../img/MesaDois.png';
+import MesaCinco from '../../img/MesaCinco.png';
+import MesaSeis from '../../img/MesaSeis.png';
+import MesaOito from '../../img/MesaOito.png';
+
 import LinkButton from '../Layout/LinkButton';
-import styles from './ExecReserva.module.css'
+import styles from './ExecReserva.module.css';
 
 const PictureList = [
   {
     id: 1,
     url: MesaQuatro,
-    name: "Mesa para quatro"
+    name: "Mesa para quatro",
+    disponivel: true,
   },
   {
     id: 2,
     url: MesaDois,
-    name: "Mesa para dois"
+    name: "Mesa para dois",
+    disponivel: true,
+  },
+  {
+    id: 3,
+    url: MesaCinco,
+    name: "Mesa para cinco",
+    disponivel: true,
+  },
+  {
+    id: 4,
+    url: MesaSeis,
+    name: "Mesa para seis",
+    disponivel: true,
+  },
+  {
+    id: 5,
+    url: MesaOito,
+    name: "Mesa para oito",
+    disponivel: false,
+  },
+  {
+    id: 6,
+    url: MesaDois,
+    name: "Mesa para dois",
+    disponivel: true,
+  },
+  {
+    id: 7,
+    url: MesaQuatro,
+    name: "Mesa para dois",
+    disponivel: true,
   },
 ];
 
@@ -22,8 +58,25 @@ function ExecReserva() {
   const [selectedButton, setSelectedButton] = useState(null);
 
   const handleButtonClick = (id) => {
-    setSelectedButton(id);
+    setSelectedButton(selectedButton === id ? null : id);
   };
+
+  const handleBodyClick = (e) => {
+    // Verifica se o clique ocorreu fora dos botões
+    if (!e.target.closest(".Board")) {
+      setSelectedButton(null);
+    }
+  };
+
+  useEffect(() => {
+    // Adiciona o manipulador de clique ao corpo do documento
+    document.body.addEventListener("click", handleBodyClick);
+
+    // Remove o manipulador quando o componente é desmontado
+    return () => {
+      document.body.removeEventListener("click", handleBodyClick);
+    };
+  }, []);
 
   return (
     <>
@@ -31,26 +84,35 @@ function ExecReserva() {
         {PictureList.map((picture) => (
           <button
             key={picture.id}
-            style={{ backgroundColor: "white" }}
+            style={{
+              backgroundColor:
+                selectedButton === picture.id
+                  ? 'white'
+                  : picture.disponivel
+                  ? 'azure'
+                  : 'firebrick',
+            }}
             onClick={() => handleButtonClick(picture.id)}
           >
             <img src={picture.url} alt={picture.id} />
           </button>
         ))}
       </div>
-      {selectedButton && (
-        <div className={styles.InfoPainel }
-        style={{padding: '0.5em' }}>
+      {selectedButton !== null && (
+        <div className={styles.InfoPainel} style={{ padding: '0.5em' }}>
           <p>ID: {selectedButton}</p>
-          <p>Tipo: {PictureList.find(p => p.id === selectedButton)?.name}</p>
-          <p>Disponivel:</p>
+          <p>Tipo: {PictureList.find((p) => p.id === selectedButton)?.name}</p>
+          <p>Disponivel: {PictureList.find((p) => p.id === selectedButton)?.disponivel ? 'Sim' : 'Não'}</p>
           <p>Espaço: Salão Principal</p>
+          {PictureList.find((p) => p.id === selectedButton)?.disponivel && (
+            <>
+              <p></p>
+              <p></p>
+              <LinkButton to="/FormsReserva" text="Reservar" />
+            </>
+          )}
           <p></p>
           <p></p>
-          <LinkButton to="/FormsReserva" text="Reservar"/>
-          <p></p>
-          <p></p>
-
         </div>
       )}
     </>
